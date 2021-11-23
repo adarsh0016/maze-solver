f87a4709a21eba766b6294d11db50391c8d12f40
adarsh <adarshprakash16@gmail.com>
2021-11-23 19:37:42 +0530

    added comments, adn optimized the code

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ let ROW;
 let COL;
 
 let ms;
+let MS = 40;   //delay.
 
+//pre-defined colours
 let bgColour = "#30363d";
 let obstacle_colour = "white";
 let path_colour = "  #33ccff";
@@ -13,6 +15,7 @@ let spread3_colour = "#360e0b";
 let arr = [];
 let vis = [];
 
+//for adjacent cells.
 var dRow = [-1, 0, 1, 0];
 var dCol = [0, 1, 0, -1];
 
@@ -37,14 +40,17 @@ slider.oninput = function() {
 var Speed_slider = document.getElementById("speedRange");
 var speed = document.getElementById("speedDemo");
 speed.innerHTML = Speed_slider.value;
-ms = 40 - Speed_slider.value;
+ms = MS - Speed_slider.value;
 
 Speed_slider.oninput = function() {
     speed.innerHTML = this.value;
-    ms = 40 - this.value;
+    ms = MS - this.value;
 }
 
+//Drawing tables
 function Draw(){
+
+    //initialize the matrix only if its not initialized already.(It can be initialized by the Generate() function.)
     if(arr.length == 0){
         for(let i = 0; i < ROW; i++){
             let arr1 = [];
@@ -55,6 +61,7 @@ function Draw(){
         }
     }
 
+    //if a table already exists, remove the table.
     var t = document.getElementById('mytable');
     if(t != null){
         t.remove();
@@ -76,13 +83,13 @@ function Draw(){
         table.appendChild(row);
     }
 
-    //making obstacles
+    //making obstacles by clicking on the cells.
     var x = document.getElementById("mytable").getElementsByTagName("td");
-    for(let i = 0; i < ROW * ROW - 1; i++){
+    for(let i = 0; i < ROW * ROW; i++){
         x[i].onclick = function(){
             if(arr[~~(i/ROW)][i%ROW] == 0){
                 x[i].style.backgroundColor = obstacle_colour;
-                arr[~~(i/ROW)][i%ROW] = 1;
+                arr[~~(i/ROW)][i%ROW] = 1;  //integer division : ~~(i/ROW).
             }
             else{
                 x[i].style.backgroundColor = bgColour;
@@ -92,26 +99,57 @@ function Draw(){
     }
 }
 
-function Start(){
-    vis = [];
-    
-    function colour(){
-        for(let i = 0; i < ROW; i++){
-            for(let j = 0; j < COL; j++){
-                if(arr[i][j] == 1){
-                    var x = document.getElementById("mytable").getElementsByTagName("td");
-                    x[i*ROW + j].style.backgroundColor = obstacle_colour;
-                }
-                else{
-                    var x = document.getElementById("mytable").getElementsByTagName("td");
-                    x[i*ROW + j].style.backgroundColor = bgColour;
-                }
+//colouring the table. (comparing it to the matrix and colour white where there is 1(i.e. an obstacle.))
+function colour(){
+    for(let i = 0; i < ROW; i++){
+        for(let j = 0; j < COL; j++){
+            var x = document.getElementById("mytable").getElementsByTagName("td");
+            if(arr[i][j] == 1){
+                x[i*ROW + j].style.backgroundColor = obstacle_colour;
+            }
+            else{
+                x[i*ROW + j].style.backgroundColor = bgColour;
             }
         }
     }
+}
 
+//it colours the spread.
+function spread_colour(path, count){
+    let l = path.length;
+    var x = document.getElementById("mytable").getElementsByTagName("td");
+    setTimeout(function(){
+        x[(path[l-1][0])*ROW + path[l-1][1]].style.backgroundColor = spread1_colour;
+    }, count * ms);
+    setTimeout(function(){
+        x[(path[l-1][0])*ROW + path[l-1][1]].style.backgroundColor = spread2_colour;
+    }, (count + 5) * ms);
+    setTimeout(function(){
+        x[(path[l-1][0])*ROW + path[l-1][1]].style.backgroundColor = spread3_colour;
+    }, (count + 10) * ms);
+    setTimeout(function(){
+        x[(path[l-1][0])*ROW + path[l-1][1]].style.backgroundColor = bgColour;
+    }, (count + 15) * ms);
+}
+
+//it colours the final path.
+function printPath(path, count){
+    for(let i = 0; i < path.length; i++){
+        var x = document.getElementById("mytable").getElementsByTagName("td");
+        setTimeout(function(){
+            x[path[i][0]*ROW + path[i][1]].style.backgroundColor = path_colour;
+        }, (count + 15) * ms + i * 10);
+    }
+}
+
+//solver function.
+function Start(){
+    vis = [];
+
+    //we have to call colour here because if we start the solver again, we need to remove the solved path.
     colour();
 
+    //checks if the adjacent cell is valid or not.
     function isValid(arr, vis, row, col){
 
         if(row < 0 || col < 0){
@@ -133,36 +171,11 @@ function Start(){
         return true;
     }
 
-    function printPath(path, count){
-        for(let i = 0; i < path.length; i++){
-            var x = document.getElementById("mytable").getElementsByTagName("td");
-            setTimeout(function(){
-                x[path[i][0]*ROW + path[i][1]].style.backgroundColor = path_colour;
-            }, (count + i/4) * ms);
-        }
-    }
-
-    function spread_colour(path, count){
-        let l = path.length;
-        var x = document.getElementById("mytable").getElementsByTagName("td");
-        setTimeout(function(){
-            x[(path[l-1][0])*ROW + path[l-1][1]].style.backgroundColor = spread1_colour;
-        }, count * ms);
-        setTimeout(function(){
-            x[(path[l-1][0])*ROW + path[l-1][1]].style.backgroundColor = spread2_colour;
-        }, (count + 5) * ms);
-        setTimeout(function(){
-            x[(path[l-1][0])*ROW + path[l-1][1]].style.backgroundColor = spread3_colour;
-        }, (count + 10) * ms);
-        setTimeout(function(){
-            x[(path[l-1][0])*ROW + path[l-1][1]].style.backgroundColor = bgColour;
-        }, (count + 15) * ms);
-    }
-
+    //the breadth first search algorithm.
     function bfs(arr, vis, row, col){
         var q = [];
 
-        if(isValid(arr, vis, row, col)){
+        if(isValid(arr, vis, row, col)){    //checking if the current cell from the queue is still valid or not (beacause once a valid cell can become eventually invalid.).
             q.push([[[row, col]],0]);
             vis[row][col] = true;
         }
@@ -170,49 +183,56 @@ function Start(){
             return false;
         }
 
-        let count = 0;
+        let count = 0;  //for delay in spread colour.
 
         while(q.length != 0){
-            var path = q[0][0];
-            count = q[0][1];
-            var c = path[path.length - 1];
+            var path = q[0][0];  //top element(path) in the queue.
+            var c = path[path.length - 1];  //the last cell in the path, to continue it further.
 
             if(c[0] == ROW - 1 && c[1] == COL - 1){
                 printPath(path, count);
                 return true;
             }
 
+            //get the time delay for that specific path.
+            count = q[0][1];
+
+            //dequeue.
             q.shift();
 
             count++;
 
+            //here we take all the adjacent cells using the defined dROW and dCOL lists.
             for(let i = 0; i < 4; i++){
                 let adjr = c[0] + dRow[i];
                 let adjc = c[1] + dCol[i];
-                let newPath = path.slice();
+                let newPath = path.slice();  //creating a copy of the path to do further calculations (because in JavaScript we cant just equal it).
 
                 if(isValid(arr, vis, adjr, adjc)){
-                    newPath.push([adjr, adjc]);
-                    q.push([newPath, count]);
+                    newPath.push([adjr, adjc]);   //adding the new explored cell in the path.
+                    q.push([newPath, count]);    //inserting the path and path delay.
 
-                    spread_colour(newPath, count);
+                    spread_colour(newPath, count);  //colour the spread.
 
-                    vis[adjr][adjc] = 1;
+                    vis[adjr][adjc] = 1;   //mark it visited.
                 }
             }
         }
-        return false;
+        return false;    //if not found any path then.
     }
 
-    for(let i = 0; i < ROW; i++){
-        let arr1 = [];
-        for(let j = 0; j < COL; j++){
-            arr1.push(0);
+    //initializing the matrix.
+    if(arr.length == 0){
+        for(let i = 0; i < ROW; i++){
+            let arr1 = [];
+            for(let j = 0; j < COL; j++){
+                arr1.push(0);
+            }
+            arr.push(arr1);
         }
-        arr.push(arr1);
     }
 
-
+    //initializing the visited matrix.
     for(let i = 0; i < ROW; i++){
         let arr1 = [];
         for(let j = 0; j < COL; j++){
@@ -224,14 +244,13 @@ function Start(){
     bfs(arr, vis, 0, 0);
 }
 
-
 //Maze generation algorithm
 function Generate(){
     Reset();
     arr = [];
     let vis_gen = [];
 
-    //visited ar initialisation
+    //visited ar initialisation (with all cells as obstacles)
     for(let i = 0; i < ROW; i++){
         let arr1 = [];
         for(let j = 0; j < COL; j++){
@@ -249,6 +268,7 @@ function Generate(){
         arr.push(arr1);
     }
 
+    //RNG for chances of connecting to other paths(i.e. making it on an invalid path.)
     function random(){
         let a = Math.random();
         if(a < 0.01){
@@ -259,6 +279,7 @@ function Generate(){
         }
     }
 
+    //checks if the adjacent path is invalid(i.e. it does not connect to another path.)
     function isValid(r, c, cell){
         if(r < 0 || c < 0){
             return false;
@@ -288,6 +309,7 @@ function Generate(){
         }
     }
 
+    //checks the end of that specific path generation (i.e. no adjacent cells are valid).
     function checkEnd(r, c){
         let ret = 0;
         
@@ -306,6 +328,7 @@ function Generate(){
             }
         }
 
+        //if yes, it still joins some paths to create multiple solutions using the random number generator.
         if(ret > 1){
             return random();
         }
@@ -323,14 +346,14 @@ function Generate(){
 
         let pick = Math.round(Math.random() * 3);
 
-        let c = s[s.length - 1];
+        let c = s[s.length - 1];    //top of the sack.
 
-        if(checkEnd(c[0], c[1])){
+        if(checkEnd(c[0], c[1])){   //if checkend is false, then pop.(i.e. it checks for a cell in the path which has valid adjacent cells.)
             s.pop();
             continue;
         }
         
-        arr[c[0]][c[1]] = 0;
+        arr[c[0]][c[1]] = 0;   //removes the obstacle in the current cell.
         vis_gen[c[0]][c[1]] = 1;
 
         s.pop();
@@ -347,21 +370,7 @@ function Generate(){
         }
     }
 
-    function colour(){
-        for(let i = 0; i < ROW; i++){
-            for(let j = 0; j < COL; j++){
-                if(arr[i][j] == 1){
-                    var x = document.getElementById("mytable").getElementsByTagName("td");
-                    x[i*ROW + j].style.backgroundColor = obstacle_colour;
-                }
-                else{
-                    var x = document.getElementById("mytable").getElementsByTagName("td");
-                    x[i*ROW + j].style.backgroundColor = bgColour;
-                }
-            }
-        }
-    }
-
+    //colour the obstacles.
     colour();
 }
 
